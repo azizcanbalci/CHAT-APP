07f358c6ceffdf53712362f9730ae5ee724f0706
Azizcan Balcı <azizcanbalci07@gmail.com>
2025-02-22 23:55:03 +0300

    completed signup page

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -1,24 +1,23 @@
-import {create} from 'zustand';
-import { axiosInstance } from '../lib/axios';
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios";
 
 export const useAuthStore = create((set) => ({
-    authUser: null,
-    issigningUp: false,
-    isLoggingIn: false,
-    isupdatingProfile: false,
+  authUser: null,
+  issigningUp: false,
+  isLoggingIn: false,
+  isupdatingProfile: false,
 
-    isCheckingAuth: true,
-    checkAuth: async () => {
-        try {
-            const res = await axiosInstance.get('auth/check');
-            set({ authUser: res.data });
-        } catch (error) {
-            console.log("Erros is checkAuth:",error);
-            set({ authUser: null });
-        }
-        finally{
-            set({isCheckingAuth: false});
-        }
-    },
-
+  isCheckingAuth: true,
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      console.log("Erros is checkAuth:", error);
+      set({ authUser: null });
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+  signup: async (data) => {},
 }));
