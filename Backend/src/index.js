0a78d15cdcbe539642b80021cb1f6870f0fe792f
Azizcan Balcı <azizcanbalci07@gmail.com>
2025-02-27 13:57:10 +0300

    completed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,9 +7,12 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
+import path from "path";
+
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -22,6 +25,13 @@ app.use(
 
 app.use("/api/auth", authroutes);
 app.use("/api/messages", messageroutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server is running on port:" + PORT);
